1a6a8742d5daa0ef8ed2ccd276093b0a0e709a81
Sumit7982 <sumit2525singh2525@gmail.com>
2026-09-21 15:18:07 +0530

    Update factorial_of_a_number.py

--- factorial_of_a_number.py
+++ factorial_of_a_number.py
@@ -1,14 +1,26 @@
+# Function to calculate factorial
 def factorial(n):
+    # Start factorial with 1
     fact = 1
 
+    # Loop from 1 to n
     for i in range(1, n + 1):
+        # Multiply fact by i
         fact = fact * i
 
+    # Return the factorial value
     return fact
 
 
+# Take a number from user
 n = int(input("Enter a number: "))
 
+# Call the function
 result = factorial(n)
 
+# Print the factorial
 print("Factorial:", result)
+
+#output
+#Enter a number: 5
+#Factorial: 120
