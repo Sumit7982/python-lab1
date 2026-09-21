d34a9f0fcd9001bb660ebab6441b59cb51f1483e
Sumit7982 <sumit2525singh2525@gmail.com>
2026-09-21 15:12:50 +0530

    Update (a)add_entry.py

--- (a)add_entry.py
+++ (a)add_entry.py
@@ -1,10 +1,20 @@
+# Function to add a new entry
 def add_entry(d):
+    # Add a new key-value pair
     d["age"] = 20
 
+# Create a dictionary
 my_dict = {"name": "Sumit"}
 
+# Print dictionary before function call
 print("Before:", my_dict)
 
+# Call the function
 add_entry(my_dict)
 
+# Print dictionary after function call
 print("After:", my_dict)
+
+#output
+#Before: {'name': 'Sumit'}
+#After: {'name': 'Sumit', 'age': 20}
