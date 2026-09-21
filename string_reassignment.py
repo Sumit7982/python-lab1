22bed7b883d0d733ceda4dbed5550fbe857000f4
Sumit7982 <sumit2525singh2525@gmail.com>
2026-09-21 15:12:10 +0530

    Update string_reassignment.py

--- string_reassignment.py
+++ string_reassignment.py
@@ -1,11 +1,24 @@
+# Function to change the first character
 def change_string(s):
+    # Create a new string with X as the first character
     s = "X" + s[1:]
+    
+    # Print the changed string inside the function
     print("Inside:", s)
 
+# Create a string
 my_string = "Hello"
 
+# Print string before function call
 print("Before:", my_string)
 
+# Call the function
 change_string(my_string)
 
+# Check the original string
 print("After:", my_string)
+
+#output
+#Before: Hello
+#Inside: Xello
+#After: Hello
